b05bc38d99307405b19045f9234e9ec6cb0d6cd0
qiaoqw <qiao_qingwen@163.com>
2019-12-29 19:37:42 +0800

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+# import need manager module
+import MongoUtil
+import FileUtil
+import conf_dev
+import conf_test
+import scratch_airport_name
+import scratch_movie_name
+import MainUtil
---
+++ conf_dev.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+# the configuration file of develop environment
+
+# path configure
+data_root_path = 'D:/node/python/smart/data'
+
+# mongodb configure
+
+user = "qiaoqw"
+pwd = "123456"
+server = "192.168.192.128"
+port = "27017"
+db_name = "smartdb"
